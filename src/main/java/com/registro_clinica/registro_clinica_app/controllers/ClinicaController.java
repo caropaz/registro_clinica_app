1e90f8fb35786c4f5871199b0f5952430d947296
Carolina <pazcaro979@gmail.com>
2024-02-20 16:10:10 -0300

    se agrega el metodo listar y se inyecta la clase service

--- src/main/java/com/registro_clinica/registro_clinica_app/controllers/ClinicaController.java
+++ src/main/java/com/registro_clinica/registro_clinica_app/controllers/ClinicaController.java
@@ -1,15 +1,28 @@
 package com.registro_clinica.registro_clinica_app.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.registro_clinica.registro_clinica_app.entities.Paciente;
+import com.registro_clinica.registro_clinica_app.services.PacienteService;
 
 @RestController
 @RequestMapping("/clinica")
 public class ClinicaController {
 
+    @Autowired
+    private PacienteService service;
     @GetMapping("/")
     public String home(){
         return "Pagina Principal";
+    }
+
+    @GetMapping("/listar")
+    public List<Paciente> listarPacientes(){
+        return service.findAll();
     }
 }
